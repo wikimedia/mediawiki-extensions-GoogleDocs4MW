fc2c010b86b1bdbb1245847ed216ca1e56b23d8a
Jack Phoenix <jack@countervandalism.net>
2015-11-22 14:50:59 +0200

    Version 1.2: refactoring, extension registration support
    Change-Id: Ic65c21e3a0522f7c61207647db86454438eb6215

--- GoogleDocs4MW.php
+++ GoogleDocs4MW.php
@@ -5,45 +5,21 @@
  *
  * @file
  * @ingroup Extensions
- * @version 1.1
+ * @version 1.2
  * @author Jack Phoenix <jack@shoutwiki.com>
- * @copyright © 2008-2011 Jack Phoenix
+ * @copyright © 2008-2015 Jack Phoenix
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
-
-if( !defined( 'MEDIAWIKI' ) ) {
-	die( "This file is an extension to the MediaWiki software and is not a valid access point.\n" );
-}
 
 // Add extension credits that show up on Special:Version
 $wgExtensionCredits['parserhook'][] = array(
 	'name' => 'GoogleDocs4MW',
-	'version' => '1.1',
+	'version' => '1.2',
 	'author' => 'Jack Phoenix',
-	'description' => 'Adds <tt>&lt;googlespreadsheet&gt;</tt> tag for Google Docs\' spreadsheets display',
+	'description' => 'Adds <code>&lt;googlespreadsheet&gt;</code> tag for Google Docs\' spreadsheets display',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:GoogleDocs4MW'
 );
 
 // Set up the parser hook
-$wgHooks['ParserFirstCallInit'][] = 'wfGoogleDocs';
-function wfGoogleDocs( &$parser ) {
-	$parser->setHook( 'googlespreadsheet', 'renderGoogleSpreadsheet' );
-	return true;
-}
-
-// The callback function for converting the input to HTML output
-function renderGoogleSpreadsheet( $input, $argv ) {
-	$width = isset( $argv['width'] ) ? $argv['width'] : 500;
-	$height = isset( $argv['height'] ) ? $argv['height'] : 300;
-	$style = isset( $argv['style'] ) ? $argv['style'] : 'width:100%';
-	$key = htmlspecialchars( $input );
-
-	$output = '<iframe class="googlespreadsheetframe" width="' .
-				intval( $width ) . '" height="' .
-				intval( $height ) . '" style="' .
-				htmlspecialchars( $style ) .
-				'" src="http://spreadsheets.google.com/pub?key=' . $key .
-				'&output=html&widget=true"></iframe>';
-
-	return $output;
-}
+$wgAutoloadClasses['GoogleDocs4MW'] = __DIR__ . '/GoogleDocs4MW.class.php';
+$wgHooks['ParserFirstCallInit'][] = 'GoogleDocs4MW::registerTag';
