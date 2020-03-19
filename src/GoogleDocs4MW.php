183ffa0d44c767097d816ddaa3ac0c688c60dfd4
zoranzoki21 <zorandori4444@gmail.com>
2020-03-19 16:07:36 +0100

    Add phpcs and make it pass
    Change-Id: I5e81a7417e10d29c1285bcf95d5b6b2bc4d4f5aa

--- src/GoogleDocs4MW.php
+++ src/GoogleDocs4MW.php
@@ -8,16 +8,25 @@
  * @version 1.2
  * @author Jack Phoenix
  * @copyright © 2008-2019 Jack Phoenix
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @license GPL-2.0-or-later
  */
 
 class GoogleDocs4MW {
 
-	public static function registerTag( &$parser ) {
+	/**
+	 * @param Parser $parser
+	 */
+	public static function registerTag( $parser ) {
 		$parser->setHook( 'googlespreadsheet', [ __CLASS__, 'renderGoogleSpreadsheet' ] );
 	}
 
-	// The callback function for converting the input to HTML output
+	/**
+	 * The callback function for converting the input to HTML output
+	 *
+	 * @param string $input
+	 * @param array $argv
+	 * @return $output
+	 */
 	public static function renderGoogleSpreadsheet( $input, $argv ) {
 		$width = isset( $argv['width'] ) ? $argv['width'] : 500;
 		$height = isset( $argv['height'] ) ? $argv['height'] : 300;
